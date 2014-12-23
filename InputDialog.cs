9a44606a37b833f1b73dd8780572458b7a0f67dd
wboka <wboka@users.noreply.github.com>
2014-12-23 14:16:55 -0500

    Adding comments

--- InputDialog.cs
+++ InputDialog.cs
@@ -6,43 +6,72 @@ namespace DialogTest
     {
         public static DialogResult Show(ref string input, string prompt = "", string confirmText = "&OK", string cancelText = "&Cancel", int w = 200, int h = 70)
         {
-            System.Drawing.Size size = new System.Drawing.Size(w, h);
+            // Create the form
             Form inputBox = new Form();
 
+            // Set the form border style
             inputBox.FormBorderStyle = FormBorderStyle.FixedDialog;
+            // Set size of the form
+            System.Drawing.Size size = new System.Drawing.Size(w, h);
             inputBox.ClientSize = size;
+            // Set the prompt
             inputBox.Text = prompt;
+            // Set the form's starting position
             inputBox.StartPosition = FormStartPosition.CenterParent;
 
-            System.Windows.Forms.TextBox textBox = new TextBox();
+            // Create an input box
+            TextBox textBox = new TextBox();
+            // Set the input's size
             textBox.Size = new System.Drawing.Size(size.Width - 10, 23);
+            // Set the input's location
             textBox.Location = new System.Drawing.Point(5, 5);
+            // Set the text to the string passed in as input
             textBox.Text = input;
+            // Add the input to the form
             inputBox.Controls.Add(textBox);
 
-            Button okButton = new Button();
-            okButton.DialogResult = DialogResult.OK;
-            okButton.Name = "okButton";
-            okButton.Size = new System.Drawing.Size(75, 23);
-            okButton.Text = confirmText;
-            okButton.Location = new System.Drawing.Point(size.Width - 80 - 80, 39);
-            inputBox.Controls.Add(okButton);
+            // Create a confirm button
+            Button confirmButton = new Button();
+            // Set the dialog result to OK
+            confirmButton.DialogResult = DialogResult.OK;
+            // Set the confirm button's name
+            confirmButton.Name = "confirmButton";
+            // Set the confirm button's size
+            confirmButton.Size = new System.Drawing.Size(75, 23);
+            // Set the confirm button's text
+            confirmButton.Text = confirmText;
+            // Set the confirm button's location
+            confirmButton.Location = new System.Drawing.Point(size.Width - 80 - 80, 39);
+            // Add the confirm button to the form
+            inputBox.Controls.Add(confirmButton);
 
+            // Create a cancel button
             Button cancelButton = new Button();
+            // Set the dialog result to Cancel
             cancelButton.DialogResult = DialogResult.Cancel;
+            // Set the cancel button's name
             cancelButton.Name = "cancelButton";
+            // Set the cancel button's size
             cancelButton.Size = new System.Drawing.Size(75, 23);
+            // Set the cancel button's text
             cancelButton.Text = cancelText;
+            // Set the cancel button's location
             cancelButton.Location = new System.Drawing.Point(size.Width - 80, 39);
+            // Add the cancel button to the form
             inputBox.Controls.Add(cancelButton);
 
-            inputBox.AcceptButton = okButton;
+            // Set the accept button to the confirm button
+            inputBox.AcceptButton = confirmButton;
+            // Set the cancel button to the cancel button
             inputBox.CancelButton = cancelButton;
 
+            // Get the dialog result from the form
             DialogResult result = inputBox.ShowDialog();
 
+            // Set the input to the text input's text if the user click the confirm button otherwise set it to null
             input = result == DialogResult.OK ? textBox.Text : null;
 
+            // Return the dialog result
             return result;
         }
     }
